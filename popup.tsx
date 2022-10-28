61bec1280fa0594f8ff0282e6c6de029fde7bf66
ChengZhenghao <chengzhenghao@kuaishou.com>
2022-10-28 19:24:43 +0800

    feat(start): add plasmo

---
+++ popup.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+     123
+    </div>
+  )
+}
+
+export default IndexPopup
